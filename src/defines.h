f6024c1b66774a754b86666578204d471be94243
François <81768287+guillermfr@users.noreply.github.com>
2022-12-11 15:39:25 +0100

    Small change

--- src/defines.h
+++ src/defines.h
@@ -4,6 +4,6 @@
 // Commande permettant de vider le terminal
 #define CLEAR_CONSOLE printf("\e[1;1H\e[2J");
 
-#define SEPARATION_AFFICHAGE_FILE printf("-------------------------------\n");
+#define SEPARATION_AFFICHAGE_FILE printf("--------------------------------------------\n");
 
 #endif
